3c0f2f0946854b1ca90591df8bd2d63dd6b11ba7
GitHub Action <action@github.com>
2025-09-05 14:56:47 +0000

    Release v1.0.2 - Fri Sep  5 14:56:47 UTC 2025

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import "dotenv/config";
+import { MeetgeekMcpServer } from "./server.js";
+
+async function main() {
+    const server = new MeetgeekMcpServer();
+    await server.start();
+}
+
+main().catch((error) => {
+    console.error("Application failed to start:", error);
+    process.exit(1);
+});
